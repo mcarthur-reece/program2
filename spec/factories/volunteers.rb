9fde48578b04baf30071d39d4645b2c8da6ddc7f
cjbhatna_ncstate <cjbhatna@ncsu.edu>
2026-02-17 20:07:03 -0500

    Added volunteer factory

---
+++ spec/factories/volunteers.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :volunteer do
+    sequence(:username) { |n| "volunteer#{n}" }
+    password { "password123" }
+    password_confirmation { "password123" }
+    sequence(:full_name) { |n| "Volunteer User #{n}" }
+    sequence(:email) { |n| "volunteer#{n}@example.com" }
+    phone_number { "123-456-7890" }
+    address { "123 Main St, City, State 12345" }
+    skills_interests { "Community service, teaching, organizing events" }
+    total_hours { 0.0 }
+  end
+end
